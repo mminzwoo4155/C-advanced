d433244333487bf5ded2a9189ef951cf59aff6c6
mminzwoo4155 <80813165+mminzwoo4155@users.noreply.github.com>
2021-06-15 16:53:32 +0700

    Need to add AnhViet.dat and AnhViet.txt to run

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    printf("Hello\n");
+    scanf("%ld", &i);
+
+    printf("%ld", i);
+
+    return 0;
+}
---
+++ testarray.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+int main ()
+{
+    char arr[5] = "12345";
+    printf ("%s\n", arr);
+    return 0;
+}
